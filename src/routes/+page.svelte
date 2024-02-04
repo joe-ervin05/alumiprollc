fdc25e767359d61873fb9eb374b7db8a04819086
Joe Ervin <joeerv123@outlook.com>
2024-02-04 15:20:06 -0500

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,21 +30,21 @@
         <p class="theme-p">A collection of our best work.</p>
     </header>
     <section class="items-center justify-center grid sm:grid-cols-2 lg:grid-cols-3 w-[282px] sm:w-[572px] lg:w-[862px] gap-2 mx-4">
-        <img src="leafblower.jpg" class="shadow-lg" alt="roof work">
-        <img src="spraying-cage.jpg" class="shadow-lg" alt="">
+        <img src="leafblower.jpg" class="shadow-lg" loading="lazy" alt="roof work">
+        <img src="spraying-cage.jpg" class="shadow-lg" loading="lazy" alt="">
         <div>
-            <img src="blowing-gutter.jpg" class="w-[282px] sm:w-full sm:h-[246px] shadow-lg" alt="">
-            <img src="house-above.jpg" class="w-[282px] sm:w-full sm:h-[246px] mt-2 shadow-lg" alt="">
+            <img src="blowing-gutter.jpg" loading="lazy" class="w-[282px] sm:w-full sm:h-[246px] shadow-lg" alt="">
+            <img src="house-above.jpg" loading="lazy" class="w-[282px] sm:w-full sm:h-[246px] mt-2 shadow-lg" alt="">
         </div>
-        <img src="screen-above.jpg" class="hidden lg:block" alt="">
-        <img src="blowing-roof.jpg" class="hidden lg:block" alt="">
-        <img src="above-rotated.jpg" class="hidden lg:block" alt="">
+        <img src="screen-above.jpg" loading="lazy" class="hidden lg:block" alt="">
+        <img src="blowing-roof.jpg" loading="lazy" class="hidden lg:block" alt="">
+        <img src="above-rotated.jpg" loading="lazy" class="hidden lg:block" alt="">
         <div>
-            <img src="ladder-on-house.jpg" class="w-[282px] sm:w-full sm:h-[246px] shadow-lg" alt="">
-            <img src="spraying-roof.jpg" class="w-[282px] sm:w-full sm:h-[246px] mt-2 shadow-lg" alt="">
+            <img src="ladder-on-house.jpg" loading="lazy" class="w-[282px] sm:w-full sm:h-[246px] shadow-lg" alt="">
+            <img src="spraying-roof.jpg" loading="lazy" class="w-[282px] sm:w-full sm:h-[246px] mt-2 shadow-lg" alt="">
         </div>
-        <img src="cage-after.jpg" class="h-[500px] shadow-lg" alt="">
-        <img src="lucas-spraying.jpg" class="h-[500px] shadow-lg" alt="">
+        <img src="cage-after.jpg" loading="lazy" class="h-[500px] shadow-lg" alt="">
+        <img src="lucas-spraying.jpg" loading="lazy" class="h-[500px] shadow-lg" alt="">
     </section>
 </section>
 
@@ -56,6 +56,7 @@
 
     img {
         border-radius: var(--rounded-container);
+        background-color: var(--color-bg-300);
     }
 
 </style>
