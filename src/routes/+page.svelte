009be90df7138aa243dc931e50b475e226d4f7d6
joe-ervin05 <joeerv123@outlook.com>
2024-04-26 17:34:31 -0400

    changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,21 +24,23 @@
 <svelte:window bind:innerWidth={width} />
 
 <section class="hero">
-    <div class="vid-cover-container">
-        <div class="vid-cover"/>
+    <div class="hidden sm:block">
+        <div class="vid-cover-container">
+            <div class="vid-cover"/>
+        </div>
+        {#if width <= 480}
+            <video class="hero-vid" bind:this={video} src="/banner-480p.mp4" playsinline autoplay loop muted/>
+        {:else if width <= 720}
+            <video class="hero-vid" bind:this={video} src="/banner-720p.mp4" playsinline autoplay loop muted/>
+        {:else}
+            <video class="hero-vid" bind:this={video} src="/banner-1080p.mp4" playsinline autoplay loop muted/>
+        {/if}
     </div>
-    {#if width <= 480}
-        <video class="hero-vid" bind:this={video} src="/banner-480p.mp4" playsinline autoplay loop muted/>
-    {:else if width <= 720}
-        <video class="hero-vid" bind:this={video} src="/banner-720p.mp4" playsinline autoplay loop muted/>
-    {:else}
-        <video class="hero-vid" bind:this={video} src="/banner-1080p.mp4" playsinline autoplay loop muted/>
-    {/if}
     {#if ready}
-    <div class="hero-content">
-        <h1><span transition:fade class="hero-header">Alumi Pro LLC</span></h1>
-        <h2 class="theme-h3 !text-slate-200 mt-4">Professional Aluminum Contractors</h2>
-        <h3 transition:fade={{ delay: 600 }} class="hero-text max-w-[500px] mt-4">Screen enclosures, rescreens/repairs, hurricane shutters, seamless gutters.</h3>
+    <div class="z-20 flex flex-col py-8 sm:py-0 items-center sm:absolute top-[calc(50%-148px)] bottom-0 right-0 left-0">
+        <h1><span transition:fade class="text-[44px] font-bold leading-relaxed sm:!text-slate-50">Alumi Pro LLC</span></h1>
+        <h2 class="theme-h3 sm:!text-slate-200 mt-4">Professional Aluminum Contractors</h2>
+        <h3 transition:fade={{ delay: 600 }} class="text-[23px] pb-2 sm:!text-slate-300 max-w-[500px] mt-4">Screen enclosures, rescreens/repairs, hurricane shutters, seamless gutters.</h3>
         <Button color="secondary" class="mt-4">Get a free estimate</Button>
         <div class="flex gap-2 mt-2">
             <Button size="sm" href="tel:2396878658" class="!w-[140px] !h-full py-1">
@@ -69,19 +71,6 @@
         text-align: center;
         position: relative;
         width: 100%;
-    }
-
-    .hero-content {
-        z-index: 20;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        position: absolute;
-        top: calc(50% - 148px);
-        left: 0;
-        right: 0;
-        padding-left: 16px;
-        padding-right: 16px;
     }
 
     .hero-vid {
@@ -115,12 +104,6 @@
         font-weight: 700;
         line-height: 110%;
         color: var(--color-text-50);
-    }
-
-    .hero-text {
-        font-size: 23px;
-        padding: 0 0 8px 0;
-        color: var(--color-text-300);
     }
 
 </style>
