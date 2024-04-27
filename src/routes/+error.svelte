fb7e5d8e0ba0761a420ae1b41c5e59969ed5d8cf
joe-ervin05 <joeerv123@outlook.com>
2024-04-26 22:28:57 -0400

    error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+
+    import { page } from "$app/stores";
+	import Button from "$lib/components/Button.svelte";
+
+</script>
+
+<main class="w-[100vw] h-[80vh] flex flex-col items-center justify-center">
+    <h1 class="theme-hero-text !text-[60px] text-center !text-red-600">{$page.status}</h1>
+    <h2 class="theme-hero-text text-center">{$page.error?.message}</h2>
+    <Button class="mt-4" href="/" color="alternative">Home page</Button>
+</main>
