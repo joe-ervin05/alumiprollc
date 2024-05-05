6a18fbf6e524e9e72457dcfb72cb168d7c322eeb
joe-ervin05 <joeerv123@outlook.com>
2024-05-05 17:10:17 -0400

    changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,43 +4,39 @@
     import { onMount } from 'svelte';
 	import Card from "$lib/components/Card.svelte";
 
-    let video : HTMLVideoElement;
-
     let width = 0;
-
-    function playVideo() {
-        if (video.paused) {
-            video.play();
-        }
-    }
 
     let ready = false;
     onMount(() => {
         ready = true;
     })
 
+    $: console.log(width);
+
 </script>
 
 <svelte:window bind:innerWidth={width} />
 
 <section class="hero">
-    <div class="hidden sm:block">
+    <div class="block">
         <div class="vid-cover-container">
             <div class="vid-cover"/>
         </div>
-        {#if width <= 480}
-            <video class="hero-vid" bind:this={video} src="/banner-480p.mp4" playsinline autoplay loop muted/>
+        {#if width <= 420}
+            <img src="/mobile-banner.webp" alt="" />
+        {:else if width <= 640}
+            <img src="/mobile-banner-lg.webp" alt="" />
         {:else if width <= 720}
-            <video class="hero-vid" bind:this={video} src="/banner-720p.mp4" playsinline autoplay loop muted/>
+            <video class="hero-vid" src="/banner-720p.mp4" playsinline autoplay loop muted/>
         {:else}
-            <video class="hero-vid" bind:this={video} src="/banner-1080p.mp4" playsinline autoplay loop muted/>
+            <video class="hero-vid" src="/banner-1080p.mp4" playsinline autoplay loop muted/>
         {/if}
     </div>
     {#if ready}
-    <div class="z-20 flex flex-col py-8 sm:py-0 mx-4 items-center sm:absolute top-[calc(50%-148px)] bottom-0 right-0 left-0">
-        <h1><span transition:fade class="text-[44px] font-bold leading-relaxed sm:!text-slate-50">Alumi Pro LLC</span></h1>
-        <h2 class="theme-h2 sm:!text-slate-200 mt-4">Professional Aluminum Contractors</h2>
-        <h3 transition:fade={{ delay: 600 }} class="text-[20px] sm:text-[22px] pb-2 sm:!text-slate-300 max-w-[500px] mt-4">Screen enclosures, rescreens/repairs, hurricane shutters, seamless gutters.</h3>
+    <div class="z-20 flex flex-col py-8 sm:py-0 mx-4 items-center absolute top-[calc(50%-148px)] bottom-0 right-0 left-0">
+        <h1><span transition:fade class="text-[44px] font-bold leading-relaxed !text-slate-50">Alumi Pro LLC</span></h1>
+        <h2 class="theme-h2 !text-slate-200 mt-4">Professional Aluminum Contractors</h2>
+        <h3 transition:fade={{ delay: 600 }} class="text-[20px] sm:text-[22px] pb-2 !text-slate-300 max-w-[500px] mt-4">Screen enclosures, rescreens/repairs, hurricane shutters, seamless gutters.</h3>
         <Button color="secondary" href="/estimate" class="mt-4">Get a free estimate</Button>
         <div class="flex gap-2 mt-2">
             <Button size="sm" href="tel:2396878658" class="!w-[140px] !h-full py-1">
@@ -79,13 +75,13 @@
 </section>
 <section class="pt-8 sm:pt-24 pb-4 sm:pb-8">
     <div class="flex justify-center flex-wrap mx-4 gap-2">
-        <div class="flex flex-wrap gap-2">
+        <div class="flex flex-wrap justify-center gap-2">
             <img src="entry1.webp" class="rounded-md shadow-md" alt="">
             <img src="entry2.webp" class="rounded-md shadow-md" alt="">
         </div>
         <img src="railing-1.webp" class="rounded-md shadow-md" alt="">
         <img src="railing-2.webp" class="rounded-md shadow-md" alt="">
-        <div class="flex flex-wrap gap-2">
+        <div class="flex flex-wrap justify-center gap-2">
             <img src="entry3.webp" class="rounded-md shadow-md" alt="">
             <img src="entry4.webp" class="rounded-md shadow-md" alt="">    
         </div>
